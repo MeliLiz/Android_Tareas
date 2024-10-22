da90d93692e3cb8c3586ba94900000f5fe11268a
Meli Blancas <melifernandez@ciencias.unam.mx>
2024-10-21 19:32:51 -0600

    Changes

--- app/src/main/java/com/example/tareas/MainActivity.java
+++ app/src/main/java/com/example/tareas/MainActivity.java
@@ -13,9 +13,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        //setContentView(R.layout.activity_main);
-        setContentView(R.layout.info_user_register);
+        //EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.info_user_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
