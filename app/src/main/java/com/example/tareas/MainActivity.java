4ac06d7be5bd451ba4f54f1a41cd8ff3301513a1
Meli Blancas <melifernandez@ciencias.unam.mx>
2024-10-21 20:07:54 -0600

    Menu

--- app/src/main/java/com/example/tareas/MainActivity.java
+++ app/src/main/java/com/example/tareas/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +17,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        //setContentView(R.layout.info_user_register);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.info_user_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflamos el menú, esto añade los elementos del menú a la action bar si está presente.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Maneja las acciones de los ítems del menú
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            // Acción para el botón de configuración
+            return true;
+        } else if (id == R.id.action_about) {
+            // Acción para el botón de 'Acerca de'
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
