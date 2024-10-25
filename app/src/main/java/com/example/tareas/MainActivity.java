d17385b9969164ddfc1c7eb89674bde185d05032
RodrigoSanchez06 <rogerxdgta@ciencias.unam.mx>
2024-10-24 19:24:47 -0600

    Practica 2 creo que ya esta xd meli checala

--- app/src/main/java/com/example/tareas/MainActivity.java
+++ app/src/main/java/com/example/tareas/MainActivity.java
@@ -1,52 +1,89 @@
 package com.example.tareas;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
+import androidx.appcompat.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //EdgeToEdge.enable(this);
-        //setContentView(R.layout.activity_main);
-        setContentView(R.layout.info_user_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_main);
+
+        // Configuración del Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Configuración del DrawerLayout y NavigationView
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // Toggle para abrir y cerrar el Drawer
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
     @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    // Inflar el menú de tres puntos en el Toolbar
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflamos el menú, esto añade los elementos del menú a la action bar si está presente.
+        // Inflar el archivo menu_main.xml
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Maneja las acciones de los ítems del menú
         int id = item.getItemId();
 
         if (id == R.id.action_settings) {
-            // Acción para el botón de configuración
+            Log.d("MainActivity", "Settings item selected from three-dot menu");
             return true;
         } else if (id == R.id.action_about) {
-            // Acción para el botón de 'Acerca de'
+            Log.d("MainActivity", "About item selected from three-dot menu");
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.view_homeworks) {
+            Log.d("MainActivity", "Ver tareas seleccionado del drawer menu");
+        } else if (id == R.id.view_resources) {
+            Log.d("MainActivity", "Ver recursos seleccionado del drawer menu");
+        } else if (id == R.id.add_homework) {
+            Log.d("MainActivity", "Asignar tarea seleccionado del drawer menu");
+        } else if (id == R.id.edit_homework) {
+            Log.d("MainActivity", "Editar tareas seleccionado del drawer menu");
+        } else {
+            Log.d("MainActivity", "Elemento no reconocido seleccionado");
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
